1040e96526c0c2d90dcf808df35c66c4f4d0305e
chauchow25 <chau.eric@gmail.com>
2014-02-02 21:54:18 -0600

    Version 1.0
    Basic Shells

--- BudgetLevel/ViewController.h
+++ BudgetLevel/ViewController.h
@@ -7,7 +7,28 @@
 //
 
 #import <UIKit/UIKit.h>
+//#import "View.h"
+
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *percentBudgetLabel;
+@property (weak, nonatomic) IBOutlet UILabel *amountSpentLabel;
+
+@property (weak, nonatomic) IBOutlet UILabel *budgetNameLabel;
+@property (weak, nonatomic) IBOutlet UIButton *addButton;
+@property (weak, nonatomic) IBOutlet UIButton *editButton;
+
+//part of drawing circle method?
+//@property (nonatomic,assign) BOOL canDraw;
+@property (weak, nonatomic) IBOutlet UIView *budgetColor;
+
+
+
+//for purpose of setting max and min and percentage. 
+@property (weak, nonatomic) IBOutlet UITextField *testEditDollarAmount;
+@property (weak, nonatomic) IBOutlet UITextField *testEditMaxAmount;
+@property (weak, nonatomic) IBOutlet UILabel *dollarSign;
+
+@property (weak, nonatomic) IBOutlet UILabel *percentSign;
 
 @end
--- BudgetLevel/ViewController.m
+++ BudgetLevel/ViewController.m
@@ -10,15 +10,99 @@
 
 @interface ViewController ()
 
+
 @end
 
 @implementation ViewController
+//@synthesize canDraw;
+
+//initialize variable
+//may need to move these to subclass budgetDetails?
+static float amountSpent = 0;
+static float percentTotal = 0;
+static float maxAmount = 0;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	// view load set up
+    
+    //set keyboards to decimal pads
+    _testEditDollarAmount.keyboardType = UIKeyboardTypeDecimalPad;
+    _testEditMaxAmount.keyboardType = UIKeyboardTypeDecimalPad;
+    
+    
+    //default the % and expense total to whatever value is already stored
+    _percentBudgetLabel.text = [NSString stringWithFormat:@"%.00f", percentTotal];
+    _amountSpentLabel.text = [NSString stringWithFormat:@"%.02f", percentTotal];
+    
+    //create the initial circle?
+    //canDraw = YES;
+    //[self.view setCanDraw:YES];
+    //[_shapeView setNeedsDisplay];
+    
+    //initialize view color as white
+    _budgetColor.backgroundColor = [UIColor colorWithRed:(1) green:(1) blue:(1) alpha:1];
+    
 }
+
+
+//draw circle method
+/*
+- (void)drawRect:(CGRect)rect {
+    if (canDraw == YES) {
+    // The color is by this line CGContextSetRGBFillColor( context , red , green , blue , alpha);
+    CGContextRef contextRef = UIGraphicsGetCurrentContext();
+    // Draw a circle (filled)
+    CGContextFillEllipseInRect(contextRef, CGRectMake(10, 10, 25, 25));
+    CGContextSetRGBFillColor(contextRef, 0, 0, 255, 1.0);
+    }
+}
+*/
+
+
+
+
+//actions when clicking out of the text field
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    //resign first responder methods
+    [self.testEditDollarAmount resignFirstResponder];
+    [self.testEditMaxAmount resignFirstResponder];
+    
+    //initialize new text
+    NSString *stringMaxAmount = self.testEditMaxAmount.text;
+    NSString *stringDollarAmount = self.testEditDollarAmount.text;
+    
+    //change amountSpentLabel
+    self.amountSpentLabel.text = stringDollarAmount;
+    
+    //only change maxAmountSpentLabel if entry is not 0
+    if((stringMaxAmount.intValue != 0) /*&& (stringDollarAmount.intValue !=0)*/) {
+        maxAmount = stringMaxAmount.intValue;
+        amountSpent = stringDollarAmount.intValue;
+        percentTotal = (amountSpent / maxAmount) * 100;
+        self.percentBudgetLabel.text = [NSString stringWithFormat:@"%.00f", percentTotal];
+        if (percentTotal<=75) {
+            _budgetColor.backgroundColor = [UIColor colorWithRed:((percentTotal/75)*1) green:(1) blue:(0) alpha:1];
+        }
+        //sets color of square
+        if (percentTotal>75 && percentTotal <=100) {
+             _budgetColor.backgroundColor = [UIColor colorWithRed:(1) green:(((100-percentTotal)/25)*1) blue:(0) alpha:1];
+        }
+        if (percentTotal >100){
+             _budgetColor.backgroundColor = [UIColor colorWithRed:(1) green:(0) blue:(0) alpha:1];
+        }
+    }
+    if(stringMaxAmount.intValue ==0){
+        self.percentBudgetLabel.text = @"";
+         _budgetColor.backgroundColor = [UIColor colorWithRed:(1) green:(1) blue:(1) alpha:1];
+    }
+    
+    
+    
+    
+}
+
 
 - (void)didReceiveMemoryWarning
 {
